011d08a6fcfc721aac0c574213fa14c920d21940
GGFalco <beppe682@gmail.com>
2023-03-10 17:47:03 +0100

    divide arrays by a factor

--- src/main/java/com/falco/exercises/arrays/DivideArray.java
+++ src/main/java/com/falco/exercises/arrays/DivideArray.java
@@ -7,7 +7,7 @@ public class DivideArray {
     public static double[] divideArray(double[] v, double factor) {
 
         double[] dst = new double[v.length];
-        for(int i = 0; i < v.length; i++){
+        for (int i = 0; i < v.length; i++) {
             dst[i] = v[i] / factor;
         }
         return dst;

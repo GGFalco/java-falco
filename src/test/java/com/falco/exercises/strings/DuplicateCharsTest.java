e10166280b9bd796ae5e388b33c842bffb8fd714
GGFalco <beppe682@gmail.com>
2023-03-11 10:11:06 +0100

    added a test

--- src/test/java/com/falco/exercises/strings/DuplicateCharsTest.java
+++ src/test/java/com/falco/exercises/strings/DuplicateCharsTest.java
@@ -10,5 +10,8 @@ class DuplicateCharsTest {
     void duplicateChars() {
         char[] ee = {'e', 'l', 'w'};
         assertArrayEquals(ee, DuplicateChars.duplicateChars("whistleblower"));
+        assertArrayEquals(new char []{'s'}, DuplicateChars.duplicateChars("ss"));
+        assertArrayEquals(new char []{}, DuplicateChars.duplicateChars("s"));
+        assertArrayEquals(new char []{}, DuplicateChars.duplicateChars(""));
     }
 }

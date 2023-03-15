3a3eae877eb5220420e42b65186d29bb255b9802
GGFalco <beppe682@gmail.com>
2023-03-15 18:01:30 +0100

    Polygon class

--- src/main/java/com/falco/exercises/oop/basic/Polygon.java
+++ src/main/java/com/falco/exercises/oop/basic/Polygon.java
@@ -2,35 +2,49 @@ package com.falco.exercises.oop.basic;
 
 import java.awt.*;
 import java.util.Arrays;
-import java.util.Random;
-import java.util.random.RandomGenerator;
 
 public class Polygon {
 
-    Point [] vertices;
+    Point[] vertices;
 
     public Polygon(Point[] vertices) {
         this.vertices = vertices;
     }
 
-    public int getVerticesCount(){
-        return vertices.length/2;
+    /**
+     * Distance beetween A(xA,yA) and B(xB,yB) is [ sqrt( (xA - xB)^2 + (yA - yB)^2 ) ]
+     *
+     * @return perimeter of a Polygon
+     */
+    public double getPerimeter() {
+        double perimeter = 0.0;
+
+        for (int i = 0; i < this.vertices.length; i++) {
+            // % vertices.length perchè cosi quando arrivo a i = 3 non accedo alla posizione 4 che non c'è nei vertici
+            // ma accedo alla (3+1)%4 = 0 ==> cioè torno al vertice iniziale
+            int next = (i + 1) % this.vertices.length;
+            perimeter += Math.hypot(vertices[i].x - vertices[next].x, vertices[i].y - vertices[next].y);
+        }
+
+        return perimeter;
     }
 
-    public Point[] getVertices() {
-        return vertices;
+    public double getArea() {
+        double area = 0.0;
+        for (int i = 0; i < vertices.length; i++) {
+            int next = (i + 1) % this.vertices.length;
+            area += (vertices[i].x * vertices[next].y) - (vertices[next].x * vertices[i].y);
+        }
+        return Math.abs(area / 2.0);
     }
 
-    public void setVertices(Point[] vertices) {
-        this.vertices = vertices;
+    public int getVerticesCount() {
+        return vertices.length;
     }
 
     @Override
     public String toString() {
-        return "Polygon{" + "vertices=" + Arrays.toString(vertices) + '}';
-    }
 
-    public static void main(String[] args) {
-        RandomGenerator rnd = new RandomGenerator();
+        return "Polygon{" + "vertices=" + Arrays.toString(vertices) + '}';
     }
 }

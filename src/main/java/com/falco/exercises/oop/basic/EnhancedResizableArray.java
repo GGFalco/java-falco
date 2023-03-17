0267ed361451baa640a7c2df2c0df1680d9eb9f5
GGFalco <beppe682@gmail.com>
2023-03-17 16:14:20 +0100

    changed set

--- src/main/java/com/falco/exercises/oop/basic/EnhancedResizableArray.java
+++ src/main/java/com/falco/exercises/oop/basic/EnhancedResizableArray.java
@@ -11,48 +11,50 @@ public class EnhancedResizableArray {
     /**
      * Create an empty array with size 0, but allocated by DEFAULT_CAPACITY
      */
-    public EnhancedResizableArray(){
+    public EnhancedResizableArray() {
         this.v = new int[DEFAULT_CAPACITY];
         this.size = 0;
     }
 
-    public void add(int value){
+    public void add(int value) {
         // se size > DEFAULT_CAPACITY = v.length
-        if(size >= v.length){
+        if (size >= v.length) {
             // creo un vettore temporanea la cui grandezza è DEFAULT_CAPACITY *2
-            int [] tmp = new int[v.length*2];
+            int[] tmp = new int[v.length * 2];
             // faccio una copia del vettore v in tmp
-            System.arraycopy(v,0,tmp,0,v.length);
+            System.arraycopy(v, 0, tmp, 0, v.length);
             // dico che v è uguale a tmp
             v = tmp;
         }
         // size è la lunghezza che incremento dopo aver aggiunto un elemento alla fine (indice size)
         set(size++, value);
     }
+
     // per me qua c'è un errore
-    public void remove(int index){
+    public void remove(int index) {
         // prendo index + 1 e lo sovrascrivo a partire da index fino alla fine
-        System.arraycopy(v,index + 1, v, index, v.length - index - 1);
+        System.arraycopy(v, index + 1, v, index, v.length - index - 1);
         size--;
     }
 
-    public int get(int index){
+    public int get(int index) {
         return v[index];
     }
 
-    public void set(int index, int value){
-        v[index] = value;
+    public void set(int index, int value) {
+        if (index < size)
+            v[index] = value;
     }
 
-    public boolean contains(int value){
+    public boolean contains(int value) {
         return Arrays.binarySearch(v, value) > 0;
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public int[] toArray(){
-        return Arrays.copyOf(v,size);
+    public int[] toArray() {
+        return Arrays.copyOf(v, size);
     }
 }

6a40efe324b2bcad127ebe3664ecc91362df1cb7
GGFalco <beppe682@gmail.com>
2023-04-27 19:04:53 +0200

    Collectors.groupingBy() e averagingDouble

--- src/main/java/com/falco/exercises/functional/PayOnAverage.java
+++ src/main/java/com/falco/exercises/functional/PayOnAverage.java
@@ -7,7 +7,7 @@ public class PayOnAverage {
 
 
     public static Map<String, Double> payOnAverage(Map<String, Double> fees){
-        fees.entrySet().stream()
+        return fees.entrySet().stream()
                 .collect(Collectors.groupingBy(
                         entry -> entry.getKey().substring(0,3),
                         Collectors.averagingDouble(entry -> entry.getValue())
